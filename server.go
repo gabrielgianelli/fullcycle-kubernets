4a5ac418ea39b6163f950e861d0e5188388c5675
Gabriel Gianelli <gabrielgianelli@gmail.com>
2022-06-07 22:29:35 -0300

    feat: :sparkles: add configmap function

--- server.go
+++ server.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
-import "net/http" 
-import "os"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http" 
+	"os"
+)
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8080", nil)
 }
@@ -13,4 +18,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/mygames/games.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "My Games: %s.", string(data))
 }
